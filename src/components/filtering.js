e3afd2d576cb2d1fe2a8fa7fa57e2f48924b7287
Bounty <kislicyn.sema@yandex.ru>
2026-03-05 12:58:50 +0300

    Для повышения функциональности и удобства использования были переработаны компоненты фильтрации, пагинации, поиска и сортировки c использованем API.

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,56 +1,42 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
-
-// @todo: #4.3 — настроить компаратор
-const compare = createComparison(defaultRules);
-
-export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
-    Object.keys(indexes).forEach((elementName) => {
-            elements[elementName].append(
-                ...Object.values(indexes[elementName]).map((name) => {
-        const option = document.createElement("option");
-        option.value = name;
-        option.textContent = name;
-        return option;
-      })
-    );
+export function initFiltering(elements) {
+    const updateIndexes = (elements, indexes) => {
+        Object.keys(indexes).forEach((elementName) => {
+            elements[elementName].append(...Object.values(indexes[elementName]).map(name => {
+                const el = document.createElement('option');
+                el.textContent = name;
+                el.value = name;
+                return el;
+            }));
         });
+    };
 
-    return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
+    const applyFiltering = (query, state, action) => {
         if (action && action.name === 'clear') {
-        const fieldName = action.dataset.field;
-        if (fieldName) {
-            const parent = action.parentElement;
-            const input = parent.querySelector('input');
-            if (input) {
-                input.value = "";
-                state[fieldName] = "";
+            const fieldName = action.dataset.field;
+            if (fieldName) {
+                const parent = action.parentElement;
+                const input = parent.querySelector('input');
+                if (input) {
+                    input.value = '';
+                    state[fieldName] = '';
                 }
             }
         }
 
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data.filter((row) => {
-            const standardMatch = compare(row, state);
-            let rangeMatch = true;
-
-            if (state.totalFrom && state.totalFrom.trim() !== '') {
-                const min = Number(state.totalFrom);
-                const value = Number(row.total);
-                if (!isNaN(min) && !isNaN(value)) {
-                    rangeMatch = rangeMatch && (value >= min);
+        const filter = {};
+        Object.keys(elements).forEach(key => {
+            if (elements[key]) {
+                if (['INPUT', 'SELECT'].includes(elements[key].tagName) && elements[key].value) {
+                    filter[`filter[${elements[key].name}]`] = elements[key].value;
                 }
             }
-
-            if (state.totalTo && state.totalTo.trim() !== '') {
-                const max = Number(state.totalTo);
-                const value = Number(row.total);
-                if (!isNaN(max) && !isNaN(value)) {
-                    rangeMatch = rangeMatch && (value <= max);
-                }
-            }
-            return standardMatch && rangeMatch;
         });
+
+        return Object.keys(filter).length ? Object.assign({}, query, filter) : query;
+    };
+
+    return {
+        updateIndexes,
+        applyFiltering
     };
 }
